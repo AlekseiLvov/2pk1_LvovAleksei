11ed49e883c74a4652c7ed2e9c6d8c717d4885d4
1 <1@DESKTOP-UD6IV01>
2021-09-30 20:29:43 +0500

    Добавьте файлы проекта.

---
+++ pz_5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace pz_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, m;
+            char smv = 'N';
+            Console.WriteLine("Задание 1");
+            for (i = -100; i <= 50; i = i + 5)
+            {
+                Console.WriteLine(i + "");
+            }
+            Console.WriteLine("Задание 2");
+            n = 0;
+            while (n <= 5)
+            {
+                n += 1;
+                Console.WriteLine(smv + "");
+                smv++;
+
+            }                                                       
+            Console.WriteLine("Задание 3");
+            {
+                n = 5;
+                m = 4;
+                for (n = 0; n <= 5; n++)
+                {
+                    for (m = 0; m <= 4; m++)
+                    {
+                        Console.Write('#');
+                    }
+                    Console.WriteLine();
+                }
+             Console.WriteLine("Задание 4");
+                for (i = 0; i <= 100; i++)
+                {
+                    if (i == 0) continue;
+                    if ((i % 7) != 0) continue;
+                    Console.WriteLine(i + "");
+                }
+
+
+            }
+        }
+    }
+}
