8d167f1e57c12691934d62b6836931dd056b611e
АЛЕКСЕЙ Львов <alesha695@gmail.com>
2021-10-01 13:55:49 +0500

    required

---
+++ pz_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            int n = int.Parse(Console.ReadLine());
+            do
+            {
+                n = n / 2;
+                k++;
+            } while (n != 0);
+            Console.WriteLine(k - 1);
+        }
+    }
+}
