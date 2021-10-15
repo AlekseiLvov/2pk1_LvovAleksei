68d56f6794890d00f39fb67c1b2245ded6c2b466
АЛЕКСЕЙ Львов <alesha695@gmail.com>
2021-10-15 15:32:14 +0500

    required

---
+++ pz_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] A = new int[n];
+            Random rnd = new Random();
+            int rndnumber;
+            double aver = 0.0;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rnd.Next(100);
+                if (A[i] < min)
+                    min = A[i];
+                if (A[i] > max)
+                    max = A[i];
+                rndnumber = rnd.Next(1, 10);
+                A[i] = rndnumber;
+                aver += A[i] / (double)A.Length;
+            }
+            Console.WriteLine("Сумма чисел массива: {0}", min + max);
+            Console.WriteLine("Произведение чисел массива: {0}", min * max);
+            Console.WriteLine("Среднее арифметическое: {0}", aver);
+            Console.ReadLine();
+        }
+
+    }      
+}
