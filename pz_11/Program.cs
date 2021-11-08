fafb317393cd7b077a3501be70053117d1ed4b7e
АЛЕКСЕЙ Львов <alesha695@gmail.com>
2021-11-08 21:23:02 +0500

    Required

---
+++ pz_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 25;
+            String S = "Преобразовать строку";
+            if (S.Length > N)
+                S = S.Substring(S.Length - N, N);
+            else
+                S = new string('.', N - S.Length) + S;
+            Console.WriteLine(S);
+            Console.ReadLine();
+        }
+    }
+}
