20b4861ca560792b1d4c3220e6641391660a2398
АЛЕКСЕЙ Львов <alesha695@gmail.com>
2021-11-13 17:57:06 +0500

    Required

---
+++ pz_13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pz_13
+{
+    class Program
+    {
+        enum grades
+        {
+            Verybad = 1, Bad = 2, Notbad = 3, Good = 4, Great = 5,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите отметку: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch ((grades)num)
+            {
+                case grades.Verybad:
+                    Console.WriteLine("Очень плохо");
+                    break;
+                case grades.Bad:
+                    Console.WriteLine("Плохо");
+                    break;
+                case grades.Notbad:
+                    Console.WriteLine(" Неплохо");
+                    break;
+                case grades.Good:
+                    Console.WriteLine("Хорошо");
+                    break;
+                case grades.Great:
+                    Console.WriteLine("Отлично");
+                    break;
+
+            }
+        }
+    }
+}
